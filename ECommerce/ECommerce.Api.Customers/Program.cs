e8ee1086d43778e24e4f0e36f543887d546c956b
recantoilhamar <glenioxg@gmail.com>
2023-10-05 12:19:46 -0300

    Add Customers microservices.

--- ECommerce/ECommerce.Api.Customers/Program.cs
+++ ECommerce/ECommerce.Api.Customers/Program.cs
@@ -1,6 +1,14 @@
+using ECommerce.Api.Customers.Db;
+using ECommerce.Api.Customers.Interfaces;
+using ECommerce.Api.Customers.Providers;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ICustomersProvider, CustomersProvider>();
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddDbContext<CustomersDbContext>(options => options.UseInMemoryDatabase("Customers"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
