3f543cc52f85427780fe164e54c009303f437f26
recantoilhamar <glenioxg@gmail.com>
2023-10-06 09:36:36 -0300

    Add Search service.

---
+++ ECommerce/ECommerce.Api.Search/Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using ECommerce.Api.Search.Interfaces;
+using ECommerce.Api.Search.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Api.Search.Controllers
+{
+    [ApiController]
+    [Route("api/search")]
+    public class SearchController : ControllerBase
+    {
+        private readonly ISearchService searchService;
+
+        public SearchController(ISearchService searchService)
+        {
+            this.searchService = searchService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SearchAsync(SearchTerm term)
+        {
+            var (IsSuccess, SearchResults) = await searchService.SearchAsync(term.CustomerId);
+
+            if (IsSuccess)
+            {
+                return Ok(SearchResults);
+            }
+
+            return NotFound();
+        }
+    }
+}
