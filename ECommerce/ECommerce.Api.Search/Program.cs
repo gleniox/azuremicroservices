9a2c430ed888a826b91f0a9f56324587136c437c
recantoilhamar <glenioxg@gmail.com>
2023-10-11 10:20:41 -0300

    Communication between Microservices

--- ECommerce/ECommerce.Api.Search/Program.cs
+++ ECommerce/ECommerce.Api.Search/Program.cs
@@ -1,15 +1,26 @@
 using ECommerce.Api.Search.Interfaces;
 using ECommerce.Api.Search.Services;
+using Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<ISearchService, SearchService>();
 builder.Services.AddScoped<IOrdersService, OrdersService>();
+builder.Services.AddScoped<IProductsService, ProductsService>();
+builder.Services.AddScoped<ICustomersService, CustomersService>();
 builder.Services.AddHttpClient("OrdersService", config =>
 {
     config.BaseAddress = new Uri(builder.Configuration["Services:Orders"]);
-});
+}).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
+builder.Services.AddHttpClient("ProductsService", config =>
+{
+    config.BaseAddress = new Uri(builder.Configuration["Services:Products"]);
+}).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
+builder.Services.AddHttpClient("CustomersService", config =>
+{
+    config.BaseAddress = new Uri(builder.Configuration["Services:Customers"]);
+}).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- ECommerce/ECommerce.Api.Search/Services/SearchService.cs
+++ ECommerce/ECommerce.Api.Search/Services/SearchService.cs
@@ -5,22 +5,42 @@ namespace ECommerce.Api.Search.Services
     public class SearchService : ISearchService
     {
         private readonly IOrdersService ordersService;
+        private readonly IProductsService productsService;
+        private readonly ICustomersService customersService;
 
-        public SearchService(IOrdersService ordersService)
+        public SearchService(IOrdersService ordersService, IProductsService productsService, ICustomersService customersService)
         {
             this.ordersService = ordersService;
+            this.productsService = productsService;
+            this.customersService = customersService;
         }
 
         public async Task<(bool IsSuccess, dynamic SearchResults)> SearchAsync(int customerId)
         {
             var ordersResult = await ordersService.GetOrdersAsync(customerId);
+            var productsResult = await productsService.GetProductsAsync();
+            var customersResult = await customersService.GetCustomerAsync(customerId);
 
             if (ordersResult.IsSuccess)
             {
+
+                foreach (var order in ordersResult.Orders)
+                {
+                    foreach (var orderItem in order.Items)
+                    {
+                        orderItem.ProductName = productsResult.IsSuccess
+                            ? productsResult.Products.FirstOrDefault(p => p.Id == orderItem.ProductId)?.Name
+                            : "Product information is not available";
+                    }
+                }
+
                 var result = new
                 {
+                    Customer = customersResult.IsSuccess ? customersResult.Customer
+                            : new { Name = "Customer information is not available" },
                     Orders = ordersResult.Orders
                 };
+
                 return (true, result);
             }
 
